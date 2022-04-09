8fa51ee3e323c9605d58fe692a74ea12b6283056
Jonathan Koban <jon.koban@outlook.com>
2022-04-09 17:29:44 -0400

    commit tree a60433cf8a7143e3764aecbaa1170cefac19b57d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards (namesArray, event ) {
+  let thankYouCards =[]
+  for(let i = 0; i < namesArray.length; i++){
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+// function countDown(num) {
+//   while(num >= 0){
+//     console.log(countdown--) 
+//   }
+// }
+
+function countDown(positiveNumber) {
+  while(positiveNumber >= 0) {
+    console.log(positiveNumber--); 
+  }
+}
